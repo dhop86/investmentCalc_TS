c22b8b2eee43aa6644c5f3b9f76ae548d80823cd
Daniel <100863441+dhop86@users.noreply.github.com>
2024-07-17 17:43:29 -0400

    Delete modules directory

--- modules/utils.ts
+++
@@ -1,7 +0,0 @@
-export const GET_ELEM_BY_ID = (id: string) => {
-    return document.getElementById(id) as HTMLElement;
-};
-
-export const GET_ELEM_BY_CLASS = (id: string) => {
-    return document.getElementsByClassName(id);
-};
