710afdf229cd8154de0e527a6c2ad61dcc9bdd72
Daniel <100863441+dhop86@users.noreply.github.com>
2024-07-17 17:43:35 -0400

    Delete interfaces directory

--- interfaces/Is_Investment.ts
+++
@@ -1,8 +0,0 @@
-export interface Is_Investment {
-    initialAmount: number, 
-    years: number, 
-    monthlyContrib: number, 
-    interestRate: number,
-    calculator(): string[],
-    addRow(a: number[]): void,
-}
